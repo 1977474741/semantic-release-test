123b6a1ef8dc31efee2ba92509880f03588bcfaf
qi <qi.zhang17@zhenai.com>
2022-04-25 17:27:21 +0800

    feat:新增semantic-release配置

---
+++ index.js
@@ -0,0 +1,17 @@
+var { createCanvas } = require('canvas');
+var echarts = require('echarts');
+echarts.setCanvasCreator(createCanvas);
+
+console.log('test1');
+module.exports = {
+    init: function(width,height){
+        let w = width || 300,
+            h = height || 300;
+        if(typeof width == 'object' && width != null){
+            w = width.width || 300,
+            h = width.height || 300;
+        }
+        var canvas = createCanvas(w, h);
+        return echarts.init(canvas);
+    }
+}
