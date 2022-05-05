471beea48cbb5923be689f577c70a0813ef7bcc9
qi <qi.zhang17@zhenai.com>
2022-05-05 19:49:26 +0800

    feat: 测试semantic-release-gh-pages-plugin

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  build:{
+      outDir: 'docs/', //指定输出路径（相对于 项目根目录). 建议使用系统默认
+      // assetsDir:'', // 指定生成静态资源的存放路径（相对于 build.outDir）
+    }
 })
