2e5278fb1625e5434a810004b04ad7ce50086449
qi <qi.zhang17@zhenai.com>
2022-05-06 19:18:24 +0800

    refactor: 测试发布ci

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,10 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  build:{
-	outDir: 'docs/', //指定输出路径（相对于 项目根目录). 建议使用系统默认
-	assetsDir:'assets/', // 指定生成静态资源的存放路径（相对于 build.outDir）
+	plugins: [vue()],
+	build:{
+		outDir: 'docs/', //指定输出路径（相对于 项目根目录). 建议使用系统默认
+		assetsDir:'assets/', // 指定生成静态资源的存放路径（相对于 build.outDir）
 	},
     base: './'
 })
