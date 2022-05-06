6b2371e3e6f0a3c0395c35828c36bf14a3824ebc
qi <qi.zhang17@zhenai.com>
2022-05-06 19:15:05 +0800

    style: 测试发布ci

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build:{
-      outDir: 'docs/', //指定输出路径（相对于 项目根目录). 建议使用系统默认
-      assetsDir:'assets/', // 指定生成静态资源的存放路径（相对于 build.outDir）
-      base: '/semantic-release-test/'
-    }
+	outDir: 'docs/', //指定输出路径（相对于 项目根目录). 建议使用系统默认
+	assetsDir:'assets/', // 指定生成静态资源的存放路径（相对于 build.outDir）
+	},
+    base: './'
 })
